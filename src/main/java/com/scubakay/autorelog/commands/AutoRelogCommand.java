f32fe6cd7d9d2aa1d8b286465997c7b7520c94ad
BuildTools <kayvanbree@gmail.com>
2023-05-06 11:53:40 +0200

    #6 Add a response to the /autorelog command to tell it has been activated

--- src/main/java/com/scubakay/autorelog/commands/AutoRelogCommand.java
+++ src/main/java/com/scubakay/autorelog/commands/AutoRelogCommand.java
@@ -6,14 +6,16 @@ import com.scubakay.autorelog.util.Reconnect;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.text.Text;
 
 public class AutoRelogCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess ignoredCommandRegistryAccess) {
         dispatcher.register(ClientCommandManager.literal("autorelog").executes(AutoRelogCommand::run));
     }
 
-    private static int run(CommandContext<FabricClientCommandSource> fabricClientCommandSourceCommandContext) {
+    private static int run(CommandContext<FabricClientCommandSource> context) {
         Reconnect.activate();
+        context.getSource().getPlayer().sendMessage(Text.translatable("commands.autorelog_activated"), false);
         return 1;
     }
 }
