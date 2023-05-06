d65fe66d5e5b00844cc04d014ed6c5b1b40bcaa9
BuildTools <kayvanbree@gmail.com>
2023-05-06 12:22:13 +0200

    Close #2: Try reconnecting until it works Close #3: Add decent logging Close #4: Clean up Reconnect class

--- src/main/java/com/scubakay/autorelog/AutoRelogClient.java
+++ src/main/java/com/scubakay/autorelog/AutoRelogClient.java
@@ -5,8 +5,13 @@ import com.scubakay.autorelog.util.Reconnect;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AutoRelogClient implements ClientModInitializer {
+    public static final String MOD_ID = "autorelog";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitializeClient() {
         ClientPlayConnectionEvents.DISCONNECT.register(Reconnect::register);
--- src/main/java/com/scubakay/autorelog/util/Reconnect.java
+++ src/main/java/com/scubakay/autorelog/util/Reconnect.java
@@ -1,15 +1,13 @@
 package com.scubakay.autorelog.util;
 
+import com.scubakay.autorelog.AutoRelogClient;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ConnectScreen;
-import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.*;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkState;
 import net.minecraft.network.packet.c2s.handshake.HandshakeC2SPacket;
 import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
-import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,6 +19,7 @@ import java.util.TimerTask;
 public class Reconnect {
     private static ServerInfo server;
     private final static long DELAY = 1000 * 30;
+    private final static long PERIOD = 1000 * 5;
 
     private static boolean active = false;
 
@@ -44,23 +43,24 @@ public class Reconnect {
         t.schedule(new TimerTask() {
             @Override
             public void run() {
-                connect(MinecraftClient.getInstance(), ServerAddress.parse(server.address), server);
+                if (connect(MinecraftClient.getInstance(), ServerAddress.parse(server.address), server)) {
+                    cancel();
+                }
             }
-        }, DELAY);
+        }, DELAY, PERIOD);
     }
 
-    private static void connect(final MinecraftClient client, final ServerAddress address, @Nullable final ServerInfo info) {
-        System.out.printf("Connecting to server: %s (%s)", server.name, server.address);
+    private static boolean connect(final MinecraftClient client, final ServerAddress address, @Nullable final ServerInfo info) {
+        AutoRelogClient.LOGGER.info(String.format("Connecting to server: %s (%s)", server.name, server.address));
 
         Screen screen = client.currentScreen;
-        InetSocketAddress inetSocketAddress = null;
+        InetSocketAddress inetSocketAddress;
 
         try {
             Optional<InetSocketAddress> optional = AllowedAddressResolver.DEFAULT.resolve(address).map(Address::getInetSocketAddress);
 
             if (optional.isEmpty()) {
-                client.execute(() -> client.setScreen(new DisconnectedScreen(screen, ScreenTexts.CONNECT_FAILED, ConnectScreen.BLOCKED_HOST_TEXT)));
-                return;
+                return false;
             }
 
             inetSocketAddress = optional.get();
@@ -68,22 +68,14 @@ public class Reconnect {
             connection.setPacketListener(new ClientLoginNetworkHandler(connection, client, info, screen, false, null, Reconnect::setStatus));
             connection.send(new HandshakeC2SPacket(inetSocketAddress.getHostName(), inetSocketAddress.getPort(), NetworkState.LOGIN));
             connection.send(new LoginHelloC2SPacket(client.getSession().getUsername(), Optional.ofNullable(client.getSession().getUuidOrNull())));
+            return true;
         } catch (Exception var6) {
-            Throwable var5 = var6.getCause();
-            Exception exception3;
-            if (var5 instanceof Exception exception2) {
-                exception3 = exception2;
-            } else {
-                exception3 = var6;
-            }
-
-            System.out.println("Couldn't connect to server");
-            String string = inetSocketAddress == null ? exception3.getMessage() : exception3.getMessage().replaceAll(inetSocketAddress.getHostName() + ":" + inetSocketAddress.getPort(), "").replaceAll(inetSocketAddress.toString(), "");
-            client.execute(() -> client.setScreen(new DisconnectedScreen(screen, ScreenTexts.CONNECT_FAILED, Text.translatable("disconnect.genericReason", string))));
+            AutoRelogClient.LOGGER.info("Couldn't connect to server");
+            return false;
         }
     }
 
     private static void setStatus(Text status) {
-
+        AutoRelogClient.LOGGER.info("Reconnected to server!");
     }
 }
