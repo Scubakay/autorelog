fa39ea1f53007b30c85fba6e419d9e6cb82a22fd
Scubakay <=>
2026-01-10 18:34:32 +0100

    - Add 1.21.11 support - Use ButtonWidget.Text, introduced in 1.21.11 - Use fully qualified Text class to avoid clashing with Buttonwidget.Text

--- src/main/java/com/scubakay/autorelog/util/ReconnectButtonWidget.java
+++ src/main/java/com/scubakay/autorelog/util/ReconnectButtonWidget.java
@@ -2,34 +2,33 @@ package com.scubakay.autorelog.util;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 //? >=1.19.3 {
 import java.util.function.Supplier;
 //?}
 
-public class ReconnectButtonWidget extends ButtonWidget {
+public class ReconnectButtonWidget extends ButtonWidget/*? >=1.21.11 {*/.Text/*? }*/ {
     private final Reconnect reconnect;
 
     public ReconnectButtonWidget() {
-        super(0, 0, 200, 20, Text.empty(), (button) -> MinecraftClient.getInstance().execute(Reconnect.getInstance()::connect) /*? >=1.19.3 {*/, Supplier::get/*?}*/);
+        super(0, 0, 200, 20, net.minecraft.text.Text.empty(), (button) -> MinecraftClient.getInstance().execute(Reconnect.getInstance()::connect) /*? >=1.19.3 {*/, Supplier::get/*?}*/);
         reconnect = Reconnect.getInstance();
     }
 
     @Override
-    public Text getMessage() {
+    public net.minecraft.text.Text getMessage() {
         active = true;
-        Text message;
+        net.minecraft.text.Text message;
         if (!reconnect.isActive() || (hovered && !(reconnect.isReconnecting() && reconnect.getCountdown() <= 0))) {
-            message =  Text.translatable("autorelog.disconnectedscreen.reconnectnow").formatted(Formatting.DARK_GREEN);
+            message =  net.minecraft.text.Text.translatable("autorelog.disconnectedscreen.reconnectnow").formatted(Formatting.DARK_GREEN);
         } else if (!reconnect.isReconnecting()) {
-            message = Text.translatable("autorelog.disconnectedscreen.failed").formatted(Formatting.RED);
+            message = net.minecraft.text.Text.translatable("autorelog.disconnectedscreen.failed").formatted(Formatting.RED);
         } else if (reconnect.getCountdown() > 0) {
-            message = Text.translatable("autorelog.disconnectedscreen.countdown", reconnect.getCountdown()).formatted(Formatting.AQUA);
+            message = net.minecraft.text.Text.translatable("autorelog.disconnectedscreen.countdown", reconnect.getCountdown()).formatted(Formatting.AQUA);
         } else {
             active = false;
-            message =  Text.translatable("autorelog.disconnectedscreen.reconnecting").formatted(Formatting.DARK_GREEN);
+            message =  net.minecraft.text.Text.translatable("autorelog.disconnectedscreen.reconnecting").formatted(Formatting.DARK_GREEN);
         }
         return message;
     }
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("fabric-loom") version "1.13-SNAPSHOT" apply false
+    id("fabric-loom") version "1.14-SNAPSHOT" apply false
     //id("dev.kikugie.j52j") version "1.0.2" apply false // Enables asset processing by writing json5 files
     id("me.modmuss50.mod-publish-plugin") version "0.7.+" apply false // Publishes builds to hosting websites
 }
