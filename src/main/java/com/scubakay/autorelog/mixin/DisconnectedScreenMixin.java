5bf65df2b04a79a9a652420a46acf4938fe75052
Scubakay <=>
2026-01-12 14:56:20 +0100

    Clean up DisconnectedScreenMixin

--- src/main/java/com/scubakay/autorelog/mixin/DisconnectedScreenMixin.java
+++ src/main/java/com/scubakay/autorelog/mixin/DisconnectedScreenMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,13 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //? >= 1.20.2 {
 import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
 import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
 //?} else if >= 1.20 {
 /*import net.minecraft.client.gui.widget.GridWidget;
 import com.llamalad7.mixinextras.sugar.Local;
-import org.spongepowered.asm.mixin.Final;
 *///?} else {
-/*import org.spongepowered.asm.mixin.injection.Redirect;
-*///?}
+/*import org.spongepowered.asm.mixin.Shadow;
+/^import org.spongepowered.asm.mixin.injection.Redirect;
+^/*///?}
 
 @SuppressWarnings("UnusedMixin")
 @Mixin(DisconnectedScreen.class)
@@ -51,11 +51,7 @@ public abstract class DisconnectedScreenMixin extends Screen {
 
     //?} else if >= 1.20 {
 
-    /*@Final
-    @Shadow
-    private GridWidget grid;
-
-    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;", ordinal = 2))
+    /*@Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;", ordinal = 2))
     private void autoRelog$injectReconnectButton(CallbackInfo ci, @Local GridWidget.Adder adder) {
         if (Reconnect.getInstance().hasAddress()) {
             ButtonWidget widget = new ReconnectButtonWidget();
